86a9040b38f156b21597930dffd524593da80f22
Antonin RAFFIN <antonin.raffin@ensta.org>
2019-07-18 16:33:53 +0200

    Move imports to top level

--- stable_baselines/logger.py
+++ stable_baselines/logger.py
@@ -8,6 +8,11 @@ import tempfile
 import warnings
 from collections import defaultdict
 
+import tensorflow as tf
+from tensorflow.python import pywrap_tensorflow
+from tensorflow.core.util import event_pb2
+from tensorflow.python.util import compat
+
 DEBUG = 10
 INFO = 20
 WARN = 30
@@ -185,8 +190,12 @@ class CSVOutputFormat(KVWriter):
 
 
 def summary_val(key, value):
+    """
+    :param key: (str)
+    :param value: (float)
+    """
     kwargs = {'tag': key, 'simple_value': float(value)}
-    return self._tf.Summary.Value(**kwargs)
+    return tf.Summary.Value(**kwargs)
 
 
 def valid_value(value):
@@ -214,18 +223,11 @@ class TensorBoardOutputFormat(KVWriter):
         self.step = 1
         prefix = 'events'
         path = os.path.join(os.path.abspath(folder), prefix)
-        import tensorflow as tf
-        from tensorflow.python import pywrap_tensorflow
-        from tensorflow.core.util import event_pb2
-        from tensorflow.python.util import compat
-        self._tf = tf
-        self.event_pb2 = event_pb2
-        self.pywrap_tensorflow = pywrap_tensorflow
         self.writer = pywrap_tensorflow.EventsWriter(compat.as_bytes(path))
 
     def writekvs(self, kvs):
-        summary = self._tf.Summary(value=[summary_val(k, v) for k, v in kvs.items() if valid_value(v)])
-        event = self.event_pb2.Event(wall_time=time.time(), summary=summary)
+        summary = tf.Summary(value=[summary_val(k, v) for k, v in kvs.items() if valid_value(v)])
+        event = event_pb2.Event(wall_time=time.time(), summary=summary)
         event.step = self.step  # is there any reason why you'd want to specify the step?
         self.writer.WriteEvent(event)
         self.writer.Flush()
